9ba899e50ed7fff38a1d60607bb522f140f76f4e
Ilya Shevchenko <illyashev2905@gmail.com>
2022-01-18 11:18:52 +0200

    end

--- Kond/Kond.cpp
+++ Kond/Kond.cpp
@@ -12,6 +12,10 @@ public:
 	{
 		this->temp++;
 	}
+	void Low()
+	{
+		this->temp--;
+	}
 	int getTemp()
 	{
 		return this->temp;
@@ -19,7 +23,7 @@ public:
 };
 class Kond
 {
-	double temp;
+	int temp;
 	bool on_off;
 public:
 	Kond()
@@ -29,15 +33,27 @@ public:
 	}
 	void Hight(Room &room)
 	{
+		this->temp++;
 		while (this->temp != room.getTemp())
 		{
-			room.Hight();
+			if(room.getTemp()>this->temp)
+			room.Low();
+			else
+			{
+				room.Hight();
+			}
 		}
-		this->temp++;
 	}
-	void Low()
+	void Low(Room &room)
 	{
 		this->temp--;
+		while (room.getTemp()!=this->temp)
+		{
+			if (room.getTemp() > this->temp)
+				room.Low();
+			else
+				room.Hight();
+		}
 	}
 	void On()
 	{
@@ -50,6 +66,14 @@ public:
 	void Print()
 	{
 		cout << "Kond: " << this->on_off << "\tTemp: " << this->temp << "\n";
+	}
+	bool getOn_Off()
+	{
+		return this->on_off;
+	}
+	int getTemp()
+	{
+		return this->temp;
 	}
 };
 class Pult
@@ -66,25 +90,73 @@ public:
 	{
 		kond.Hight(room);
 	}
-	void Low(Kond& kond)
+	void Low(Kond& kond,Room &room)
 	{
-		kond.Low();
+		kond.Low(room);
 	}
 };
 
 int main()
 {
+	setlocale(0, "");
 	Kond kond;
 	Room room;
-	/*kond.Print();
-	kond.Hight();
-	kond.Print();
-	kond.On();
-	kond.Print();*/
 	Pult pult;
-	pult.On(kond);
-	pult.Hight(kond,room);
-	kond.Print();
-	cout << room.getTemp();
-
+	int vibor;
+	do
+	{
+		cout << "\t\tМеню\n";
+		cout << "1 - ВКЛ\n";
+		cout << "2 - ВЫКЛ\n";
+		cout << "3 - Повысить температуру\n";
+		cout << "4 - Понизить температуру\n";
+		cout << "5 - Вывод всей информации\n";
+		cout << "0 - Выход\n";
+		cout << "Ваш выбор: ";
+		cin >> vibor;
+		switch (vibor)
+		{
+		case 1:
+			system("cls");
+			pult.On(kond);
+			cout << "Кондиционер Вкл.\n";
+			system("pause");
+			system("cls");
+			break;
+		case 2:
+			system("cls");
+			pult.Off(kond);
+			cout << "Кондиционер Выкл.\n";
+			system("pause");
+			system("cls");
+			break;
+		case 3:
+			system("cls");
+			pult.Hight(kond, room);
+			cout << "Вы повылили температуру на один градус!\nСкоро температура в комноте станет как на кондиционере\n";
+			system("pause");
+			system("cls");
+			break;
+		case 4:
+			system("cls");
+			cout << "Вы понизили температуру на один градус!\nСкоро температура в комноте станет как на кондиционере\n";
+			pult.Low(kond, room);
+			system("pause");
+			system("cls");
+			break;
+		case 5:
+			system("cls");
+			if (kond.getOn_Off() == true)
+			{
+				cout << "Кондиционер: Вкл.\t" << "Температура кондиционера: " << kond.getTemp() << "\tТемпература в комнате: " << room.getTemp() << "\n";
+			}
+			else
+			{
+				cout << "Кондиционер: Выкл.\t" << "Температура кондиционера: " << kond.getTemp() << "\tТемпература в комнате: " << room.getTemp() << "\n";
+			}
+			system("pause");
+			system("cls");
+			break;
+		}
+	} while (vibor != 0);
 }
